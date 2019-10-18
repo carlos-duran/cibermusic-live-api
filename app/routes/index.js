52ba6bf6b6e28fdf38b044cccab6facf1ae30abc
Ivan Chavez <ivanchavez54@gmail.com>
2019-10-18 02:13:47 -0500

    Order of routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,10 @@
 module.exports = async app => {
   app.get('/', home)
 
-  app.register(require('./countries'))
-  app.register(require('./auth'))
-  app.register(require('./user'))
   app.register(require('./admin'), { prefix: '/admin' })
+  app.register(require('./auth'))
+  app.register(require('./countries'))
+  app.register(require('./user'))
 }
 
 const home = async () => 'CiberMusic Live!'
