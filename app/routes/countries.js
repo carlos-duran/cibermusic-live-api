d55489654a8592ea1506197caadb13f5841e6ee4
Carlos Duran <carlosduran.xyz@gmail.com>
2019-10-21 19:29:15 -0500

    Add countries

--- app/routes/countries.js
+++ app/routes/countries.js
@@ -3,5 +3,32 @@ module.exports = async app => {
 }
 
 async function countries() {
-  return ['Peru', 'Mexico', 'Colombia']
+  return [
+    'Argentina',
+    'Bolivia',
+    'Brasil',
+    'Chile',
+    'Colombia',
+    'Costa Rica',
+    'Cuba',
+    'Ecuador',
+    'El Salvador',
+    'Guayana Francesa',
+    'Granada',
+    'Guatemala',
+    'Guayana',
+    'Haití',
+    'Honduras',
+    'Jamaica',
+    'México',
+    'Nicaragua',
+    'Paraguay',
+    'Panamá',
+    'Perú',
+    'Puerto Rico',
+    'República Dominicana',
+    'Surinam',
+    'Uruguay',
+    'Venezuela'
+  ]
 }
