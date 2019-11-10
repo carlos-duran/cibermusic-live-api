4698d71ca8f3454989f568efce94c3e9a948c3de
anthonyleonramirez <anthonyleonramirez@hotmail.com>
2019-11-09 22:25:59 -0500

    playlistCustom

--- app/routes/user/playlistcustom.js
+++ app/routes/user/playlistcustom.js
@@ -3,11 +3,12 @@ const Playlist = require('../../models/playlistCustom')
 module.exports = async app => {
   app.get('/playlistcustom', list)
   app.post('playlistcustom', create)
+  app.put('playlistcustom', update)
 }
 
 async function list(request) {
-  return Playlist.find()
-  //   return Playlist.find({ user: request.user.id }, '-songs')
+  // return Playlist.find()
+  return Playlist.find({ user: request.user.id }, '-songs')
 }
 
 const create = {
@@ -28,5 +29,31 @@ const create = {
       name: request.body.name,
       user: request.user.id
     })
+  }
+}
+
+const update = {
+  schema: {
+    body: {
+      type: 'object',
+      required: ['name'],
+      properties: {
+        name: {
+          type: 'string',
+          maxLength: 40
+        }
+      }
+    }
+  },
+  async handler(request) {
+    return Playlist.update(
+      {
+        _id: request.params.playlistId,
+        user: request.user.id
+      },
+      {
+        times: request.Playlist.times
+      }
+    )
   }
 }
