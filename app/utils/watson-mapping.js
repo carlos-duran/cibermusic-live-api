708405dab77017c20c145cc20bc73dcad75b7483
Carlos Duran <carlosduran.xyz@gmail.com>
2019-12-02 05:04:13 -0500

    Improve chatbot

--- app/utils/watson-mapping.js
+++ app/utils/watson-mapping.js
@@ -1,8 +1,55 @@
-const actions = {
-  EditarPefil: {
-    name: 'navigate',
-    value: '/perfil'
+const getAnswer = output => {
+  let text = output.generic[0].text
+  let actions = []
+
+  if (!output.intents.length) {
+    return { text, actions }
   }
+
+  const [{ intent }] = output.intents
+  let phrase
+
+  switch (intent) {
+    case 'EditarPefil':
+      actions = [
+        {
+          name: 'navigate',
+          value: '/perfil'
+        }
+      ]
+      break
+    case 'BuscadorCanciones':
+      phrase = getSongAndArtist(output)
+      if (phrase) {
+        actions = [
+          {
+            name: 'search-play',
+            value: phrase
+          }
+        ]
+      } else {
+        text = 'Muy pronto agregaremos más canciones!'
+      }
+      break
+    default:
+      break
+  }
+  return { text, actions }
 }
 
-module.exports = actions
+module.exports = getAnswer
+
+function getSongAndArtist(output) {
+  let phrase = ''
+  if (output.entities.length) {
+    const songEntity = output.entities.find(e => e.entity === 'Songs')
+    if (songEntity) {
+      phrase = songEntity.value
+    }
+    const artistEntity = output.entities.find(e => e.entity === 'Artists')
+    if (artistEntity) {
+      phrase += ' ' + artistEntity.value
+    }
+  }
+  return phrase.trim()
+}
